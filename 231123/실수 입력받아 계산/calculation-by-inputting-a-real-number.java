45f4ae341ade648b3db5e61e72a8116f9ea246db
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2023-11-23 20:28:19 +0900

    Title: 실수 입력받아 계산, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231123/실수 입력받아 계산/calculation-by-inputting-a-real-number.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double a = nextDouble();
+        double b = nextDouble();
+
+        System.out.print(a + b);
+    }
+}
