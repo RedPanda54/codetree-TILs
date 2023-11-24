1c77ed9ff497f86df35e60b5530b2d28f27e6daa
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2023-11-24 22:51:49 +0900

    Title: 1시간 뒤 시간 출력, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 231124/1시간 뒤 시간 출력/print-one-hour-later.java
+++ 231124/1시간 뒤 시간 출력/print-one-hour-later.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
-        String[] strArr = s.split(":");
+        int[] strArr = s.split(":");
         System.out.println(strArr[0]+1 + ":" + strArr[1]);
     }
 }
