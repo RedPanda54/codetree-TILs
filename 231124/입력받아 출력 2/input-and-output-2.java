4daba9467966e304d2d1f3954162f9ae62bc9a50
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2023-11-24 22:53:53 +0900

    Title: 입력받아 출력 2, Time: 111ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 231124/입력받아 출력 2/input-and-output-2.java
+++ 231124/입력받아 출력 2/input-and-output-2.java
@@ -5,6 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         sc.useDelimiter("-"); // - 기준으로 잘라서 입력 받겠다는 뜻
         int a = sc.nextInt(); // - 기호가 나오기 전까지 입력 진행
+        int b = sc.nextInt();
         System.out.println(a + b);
     }
 }
