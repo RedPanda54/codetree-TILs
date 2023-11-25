e0332ee9664b379839c6ac5b1e3dd1bb0dd77f68
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2023-11-25 19:44:48 +0900

    Title: 두수의 곱과 몫, Time: 147ms, Memory: 10MB, Status: Passed - CodeTree

--- 231125/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
+++ 231125/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int a = in.nextInt();
         int b = in.nextInt();
-        System.out.println(a + "*" + b + "=" + a*b);
-        System.out.println(a + "/" + b + "=" + a/b);
+        System.out.println(a + " * " + b + " = " + a*b);
+        System.out.println(a + " / " + b + " = " + a/b);
     }
 }
