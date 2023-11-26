2778da156381fa4f8ec835d4bda746cfaa4d56cc
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2023-11-26 20:28:56 +0900

    Title: 큰 수에서 빼기, Time: 148ms, Memory: 10MB, Status: Passed - CodeTree

--- 231126/큰 수에서 빼기/subtract-from-large-number.java
+++ 231126/큰 수에서 빼기/subtract-from-large-number.java
@@ -6,7 +6,7 @@ public class Main {
         int a = in.nextInt();
         int b = in.nextInt();
 
-        if (a > b) {
+        if (a >= b) {
             System.out.println(a-b);
         }
         else if (a < b) {
