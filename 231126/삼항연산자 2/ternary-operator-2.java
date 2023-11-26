6fb05509ed7dfd945ba6b67c1962be629cfaff40
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2023-11-26 21:06:51 +0900

    Title: 삼항연산자 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231126/삼항연산자 2/ternary-operator-2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+
+        String pri = a == 1 ? t : f;
+        System.out.println(pri);
+    }
+}
