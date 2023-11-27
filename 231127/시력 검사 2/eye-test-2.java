7c5ce1d31a3fbb882e4323aedf26fe8cd2685d7f
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2023-11-27 13:23:06 +0900

    Title: 시력 검사 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 231127/시력 검사 2/eye-test-2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double vision = input.nextInt();
+        if (vision >= 1.0) {
+            System.out.println("High");
+        }
+        else if (vision >= 0.5) {
+            System.out.println("Middle");
+        }
+        else {
+            System.out.println("Low");
+        }
+    }
+}
