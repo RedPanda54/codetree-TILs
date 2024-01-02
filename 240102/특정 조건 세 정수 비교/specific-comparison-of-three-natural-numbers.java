848bfbbf932fb54a3218108a67d6ad9e9053ae8c
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-01-02 21:21:46 +0900

    Title: 특정 조건 세 정수 비교, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

--- 240102/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
+++ 240102/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.java
@@ -6,24 +6,15 @@ public class Main {
         int a = input.nextInt();
         int b = input.nextInt();
         int c = input.nextInt();
-        int minimum = Math.min(a, b);
-        minimum = Math.min(minimum, c);
 
-        if (a == minimum) {
-            System.out.print(1 + " ");
-        }
-        else {
-            System.out.print(0 + " ");
-        }
+        if(a <= b && a <= c)
+            System.out.print("1 ");
+        else
+            System.out.print("0 ");
 
-        if (a == b) {
-            if (a == c) {
-                System.out.print(1);
-            }
-            System.out.println(0);
-        }
-        else {
-            System.out.print(0);
-        }
+        if(a == b && b == c)
+            System.out.println("1");
+        else
+            System.out.println("0");
     }
 }
