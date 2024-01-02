84c032f327df70769b2c4dd500cdfee61ec08daa
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-01-02 21:11:15 +0900

    Title: 4가지 관계연산자, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240102/4가지 관계연산자/four-relational-operators.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        if (a >= b) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+
+        if (a > b) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+
+        if (b >= a) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+
+        if (b > a) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
