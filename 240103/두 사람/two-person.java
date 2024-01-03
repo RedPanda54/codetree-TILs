fb43f3ec7bb059a98ec846c2dff4e5c396418836
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-01-03 21:21:55 +0900

    Title: 두 사람, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240103/두 사람/two-person.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int age1 = input.nextInt();
+        String gen1 = input.nextLine();
+        
+        int age2 = input.nextInt();
+        String gen2 = input.nextLine();
+        
+        if ((age1 >= 19 && gen1 == "M") || (age2 >= 19 && gen2 == "M")) {
+            System.out.println(1);
+        }
+        else {
+            System.out.println(0);
+        }
+    }
+}
