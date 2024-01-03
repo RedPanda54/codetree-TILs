370550a567391f0720091f424d2095f85023fc9a
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-01-03 21:24:34 +0900

    Title: 두 사람, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240103/두 사람/two-person.java
+++ 240103/두 사람/two-person.java
@@ -5,11 +5,11 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int age1 = input.nextInt();
         String gen1 = input.nextLine();
-        
+
         int age2 = input.nextInt();
         String gen2 = input.nextLine();
-        
-        if ((age1 >= 19 && gen1 == "M") || (age2 >= 19 && gen2 == "M")) {
+
+        if ((age1 >= 19 && gen1.equals("M")) || (age2 >= 19 && gen2.equals("M"))) {
             System.out.println(1);
         }
         else {
