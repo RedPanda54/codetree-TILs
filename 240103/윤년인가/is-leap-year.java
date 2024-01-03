0df25e85aafbbcd6786441e6f78ee20613d9e22b
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-01-03 21:42:21 +0900

    Title: 윤년인가, Time: 128ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240103/윤년인가/is-leap-year.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int y = input.nextInt();
+
+        if (y%4 == 0) {
+            if (y%100 == 0) {
+                System.out.println("false");
+            }
+            else if (y%400 == 0) {
+                System.out.println("true");
+            }
+            else {
+                System.out.println("true");
+            }
+        }
+        else {
+            System.out.println("false");
+        }
+    }
+}
