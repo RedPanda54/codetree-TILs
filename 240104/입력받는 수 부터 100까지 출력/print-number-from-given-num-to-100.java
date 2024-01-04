599cf4d55e06607fd8eb5276ff94160883072f74
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-01-04 21:47:52 +0900

    Title: 입력받는 수 부터 100까지 출력, Time: 129ms, Memory: 10MB, Status: Passed - CodeTree

--- 240104/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.java
+++ 240104/입력받는 수 부터 100까지 출력/print-number-from-given-num-to-100.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        for (int i=n; i<100; i++) 
+        for (int i=n; i<=100; i++) 
             System.out.print(i + " ");
     }
 }
