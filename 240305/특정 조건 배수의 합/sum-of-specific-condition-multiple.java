63866f981c58aa2cf3349b218ca2dc28324db4c3
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-03-05 22:47:29 +0900

    Title: 특정 조건 배수의 합, Time: 121ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240305/특정 조건 배수의 합/sum-of-specific-condition-multiple.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+
+        //int cnt = 0;
+        int sum = 0;
+        //double average = 0.0;
+
+        for (int i = a; i <= b; i++) {
+            if (i % 5 == 0) {
+                //cnt++;
+                sum += i;
+            }
+        }
+        //average = (double) sum / cnt;
+
+        System.out.print(sum);
+        //System.out.printf("%.1f", average);
+    }
+}
