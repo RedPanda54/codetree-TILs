7fb32c9729a08ef2ec1c44e9b114b4bf2b69fcaf
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-03-07 21:38:44 +0900

    Title: 1까지 나누기, Time: 114ms, Memory: 9MB, Status: Wrong Answer - CodeTree

--- 240307/1까지 나누기/divide-by-1.java
+++ 240307/1까지 나누기/divide-by-1.java
@@ -7,7 +7,7 @@ public class Main {
         double quotient = n;
         int count = 0;
 
-        for (int i = 1; i <= quotient; i++) {
+        for (int i = 1; i <= 100; i++) {
             quotient = (double) (quotient / i);
             count++;
 
