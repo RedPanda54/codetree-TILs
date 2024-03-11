50fb3fe87529fe4934509dfa40fa1cd66b95c1e5
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-03-11 15:45:34 +0900

    Title: 합성수 판별기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240311/합성수 판별기/factorization-discriminator.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        boolean satisfied = false;
+
+        for (int i = 1; i < n / 2; i++) {
+            if (n % i == 0) {
+                satisfied = true;
+            }
+        }
+
+        if (satisfied) {
+            System.out.print("C");
+        } else {
+            System.out.print("N");
+        }
+    }
+}
