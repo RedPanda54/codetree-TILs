8e31a5acde1ddc907bcec4639f585c7468c0762d
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-03-11 15:46:11 +0900

    Title: 합성수 판별기, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

--- 240311/합성수 판별기/factorization-discriminator.java
+++ 240311/합성수 판별기/factorization-discriminator.java
@@ -6,7 +6,7 @@ public class Main {
         int n = input.nextInt();
         boolean satisfied = false;
 
-        for (int i = 1; i < n / 2; i++) {
+        for (int i = 2; i < n / 2; i++) {
             if (n % i == 0) {
                 satisfied = true;
             }
