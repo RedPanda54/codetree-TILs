a55077d84a6077a74895e12c7cc49c95933ab542
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-03-11 16:01:29 +0900

    Title: 소수 판별기, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

--- 240311/소수 판별기/prime-number-discriminator.java
+++ 240311/소수 판별기/prime-number-discriminator.java
@@ -6,7 +6,7 @@ public class Main {
         int n = input.nextInt();
         boolean satisfied = true;
 
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < n/2; i++) {
             if (n % i == 0) {
                 satisfied = false;
                 break;
