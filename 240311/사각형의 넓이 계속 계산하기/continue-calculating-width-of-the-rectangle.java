e04880d857603f0b97cb51294aefe805f977f591
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-03-11 14:27:33 +0900

    Title: 사각형의 넓이 계속 계산하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240311/사각형의 넓이 계속 계산하기/continue-calculating-width-of-the-rectangle.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int width;
+        int height;
+        String a = "A";
+
+        while (!(a.equals("C"))) {
+            width = input.nextInt();
+            height = input.nextInt();
+            a = input.next();
+            System.out.println(width * height);
+            input.nextLine();
+        }
+    }
+}
