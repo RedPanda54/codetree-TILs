2c8e90522a68022a4c83ec77f2796a055c8fb18f
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-03-12 22:13:03 +0900

    Title: 별표 출력하기 2, Time: 159ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240312/별표 출력하기 2/print-star-2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i = n; i > 0; i--) {
+            for (int j = i; j > 0; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
