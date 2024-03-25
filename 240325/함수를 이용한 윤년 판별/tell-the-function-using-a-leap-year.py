3f85fdc886716d0555e487f02d4bc9ee3b522491
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-03-25 19:58:32 +0900

    Title: 함수를 이용한 윤년 판별, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

--- 240325/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
+++ 240325/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.py
@@ -1,13 +1,10 @@
 def is_leap_year(y):
     if y % 4 != 0:
         return False
-    if y % 100 == 0 and y % 400 != 0:
+    if (y % 100) == 0 and (y % 400) != 0:
         return False
 
     return True
 
-y = int(input().strip())
-if is_leap_year:
-    print("true")
-else:
-    print("false")
+y = int(input())
+print("true" if is_leap_year(y) else "false")
