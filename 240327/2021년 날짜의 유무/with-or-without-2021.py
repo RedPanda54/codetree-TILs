f84ce288506d9d93238ca7e92a962d463d30a408
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-03-27 11:45:00 +0900

    Title: 2021년 날짜의 유무, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240327/2021년 날짜의 유무/with-or-without-2021.py
@@ -0,0 +1,16 @@
+def is_month(M):
+    return M >= 1 and M < 13
+def is_day(M, D):
+    if M == 1 or M == 3 or M == 5 or M == 7 or M == 8 or M == 10 or M == 12:
+        return D >= 1 and D < 32
+    elif M == 2 or M == 4 or M == 6 or M == 9 or M == 11:
+        return D >= 1 and D < 31
+    else:
+        return D >= 1 and D < 29
+
+M, D = map(int, input().split())
+
+if is_month(M) and is_day(M, D):
+    print("Yes")
+else:
+    print("No")
