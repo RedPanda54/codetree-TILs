dd5e4fd633ffcadbe5c2eaa4ed625a22f800a12a
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-03-29 12:51:07 +0900

    Title: 나누고 빼면서 합하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240329/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
+++ 240329/나누고 빼면서 합하기/divide-and-subtract-and-add-up.py
@@ -1,14 +1,19 @@
 def solution(A, m):
+    global m
+
     total_sum = 0
-    while m >= 1:
-        total_sum += A[m-1]
+
+    while m:
+        total_sum += A[m]
+
         if m % 2 == 1:
             m -= 1
         else:
             m //= 2
+
     return total_sum
 
 n, m = map(int, input().split())
-A = list(map(int, input().split()))
+A = [0] + list(map(int, input().split()))
 
 print(solution(A, m))
