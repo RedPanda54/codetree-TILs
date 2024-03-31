2c2c1ce5808cd472fd9a7e9a74d73a58add62ab0
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-03-31 19:55:37 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 124ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240331/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -0,0 +1,9 @@
+def max_value(a):
+    if a == 0:
+        return arr[0]
+
+    return max(max_value(a - 1), arr[a])
+    
+n = int(input())
+arr = list(map(int, input().split()))
+print(max_value(n - 1))
