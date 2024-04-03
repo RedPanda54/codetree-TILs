3b2ede6cf5842688d7828a1b4ca1c761be5d8c50
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-04-03 11:38:39 +0900

    Title: 코드네임, Time: 201ms, Memory: 24MB, Status: Passed - CodeTree

--- 240403/코드네임/code-name.py
+++ 240403/코드네임/code-name.py
@@ -11,11 +11,8 @@ for _ in range(5):
     agents.append(Agent(name, score))
 
 min_score_index = 0
-for i in range(len(agents)):
-    if i == 0:
-        continue
-    else:
-        if agents[i].score < agents[min_score_index].score:
-            min_score_index = i
+for i in range(1, len(agents)):
+    if agents[i].score < agents[min_score_index].score:
+        min_score_index = i
 
 print(f"{agents[min_score_index].name} {agents[min_score_index].score}")
