744e78807b626bcc60be02485e300992713cefa1
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-04-08 18:33:44 +0900

    Title: 왔다 갔던 구역 2, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240408/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240408/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -1,19 +1,20 @@
 n = int(input())
-OFFSET = 1000
 count = [0] * 2001
 origin_point = 1000
+
+now_position = origin_point
 
 for _ in range(n):
     dx, shift = map(str, input().split())
     dx = int(dx)
     
     if shift == 'L':
-        for i in range(origin_point, origin_point - (dx+1), -1):
+        for i in range(now_position, now_position - dx, -1):
             count[i] += 1
-        origin_point -= dx
+        now_position -= dx
     else:
-        for i in range(origin_point, origin_point + (dx+1), 1):
+        for i in range(now_position, now_position + dx, 1):
             count[i] += 1
-        origin_point += dx
+        now_position += dx
     
 print(count.count(2))
