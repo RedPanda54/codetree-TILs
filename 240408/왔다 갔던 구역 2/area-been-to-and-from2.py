23fa73af8b9a87bb28bd01636535b3455db97424
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-04-08 18:46:32 +0900

    Title: 왔다 갔던 구역 2, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 240408/왔다 갔던 구역 2/area-been-to-and-from2.py
+++ 240408/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -9,7 +9,7 @@ for _ in range(n):
     dx = int(dx)
     
     if shift == 'L':
-        for i in range(now_position, now_position - dx, -1):
+        for i in range(now_position-1 , now_position - (dx+1) , -1):
             count[i] += 1
         now_position -= dx
     else:
