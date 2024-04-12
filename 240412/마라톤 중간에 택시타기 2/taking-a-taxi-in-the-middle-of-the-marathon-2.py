a9e3b270c55e0484fc317cee15edd71cf35cc7f1
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-04-12 19:42:02 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 127ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240412/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -0,0 +1,28 @@
+def manhattan_distance(x1, y1, x2, y2):
+    return abs(x1 - x2) + abs(y1 - y2)
+
+n = int(input())
+check_point = []
+for _ in range(n):
+    x, y = map(int, input().split())
+    check_point.append((x, y))
+
+distance = 100000000
+
+index = 1
+temp = 0
+x1, y1 = check_point[0]
+while index != (n-1):
+    for i, (x, y) in enumerate(check_point):
+        if index == i:
+            continue
+        else:
+            x2, y2 = x, y
+            temp += manhattan_distance(x1, y1, x2, y2)
+            x1, y1 = x2, y2
+    distance = min(distance, temp)
+    
+    temp = 0
+    index += 1
+
+print(distance)
