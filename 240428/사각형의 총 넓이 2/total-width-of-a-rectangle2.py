e93c3dc0a36bd2a27bb02d15b7ff68edfa6ed900
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-04-28 21:54:45 +0900

    Title: 사각형의 총 넓이 2, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 240428/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
+++ 240428/사각형의 총 넓이 2/total-width-of-a-rectangle2.py
@@ -1,14 +1,20 @@
+OFFSET = 100
+
 n = int(input())
-rectangle = [[0] * (201) for _ in range(201)]
-for _ in range(n):
-    x1, y1, x2, y2 = map(int, input().split())
-    x1 += 100
-    y1 += 100
-    x2 += 100
-    y2 += 100
+rects = [tuple(map(int, input().split())) for _ in range(n)]
+area = [[0] * (201) for _ in range(201)]
+
+for x1, y1, x2, y2 in rects:
+    x1, y1 = x1 + OFFSET, y1 + OFFSET
+    x2, y2 = x2 + OFFSET, y2 + OFFSET
     for i in range(x1, x2):
         for j in range(y1, y2):
-            rectangle[i][j] = 1
+            area[i][j] = 1
 
-total_area = sum(sum(row) for row in rectangle)
+# total_area = sum(sum(row) for row in rectangle)
+total_area = 0
+for x in range(0, 201):
+    for y in range(0, 201):
+        if area[x][y]:
+            total_area += 1
 print(total_area)
