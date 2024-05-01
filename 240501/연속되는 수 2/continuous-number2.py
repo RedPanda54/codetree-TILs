ca9ec66ee0601b4ddb94dabb8c6e36f70a1230d4
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-05-01 11:45:09 +0900

    Title: 연속되는 수 2, Time: 126ms, Memory: 28MB, Status: Passed - CodeTree

--- 240501/연속되는 수 2/continuous-number2.py
+++ 240501/연속되는 수 2/continuous-number2.py
@@ -2,7 +2,14 @@ n = int(input())
 numbers = [int(input()) for _ in range(n)]
 
 count = 1
+answer = 0
 for i in range(1, n):
     if numbers[i] != numbers[i-1]:
+        answer = max(answer, count)
+        count = 1
+    else: # number[i] == numbers[i-1]
         count += 1
-print(count)
+
+answer = max(answer, count)
+
+print(answer)
