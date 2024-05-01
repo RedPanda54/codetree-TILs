3a05d3a1dab29e61df93621823e574860ac6dcf7
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-05-01 14:28:24 +0900

    Title: 연속되는 수 3, Time: 132ms, Memory: 24MB, Status: Passed - CodeTree

--- 240501/연속되는 수 3/continuous-number3.py
+++ 240501/연속되는 수 3/continuous-number3.py
@@ -1,16 +1,21 @@
 n = int(input())
 numbers = [int(input()) for _ in range(n)]
 
-count = 1 if numbers[0] < 0 else 0
+count = 1
 answer = 0
 for i in range(1, n):
     if numbers[i] > 0:
-        answer = max(answer, count)
-        count = 0
+        if numbers[i-1] < 0:
+            answer = max(answer, count)
+            count = 1
+        else:
+            count += 1
     else: # numbers[i] < 0
         if numbers[i-1] > 0:
+            answer = max(answer, count)
             count = 1
         else: # numbers[i-1] < 0:
             count += 1
 
+answer = max(answer, count)
 print(answer)
