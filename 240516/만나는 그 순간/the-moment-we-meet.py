ce00ce019eb950e8ebdc69ee829692567bf7784b
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-05-16 12:28:54 +0900

    Title: 만나는 그 순간, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240516/만나는 그 순간/the-moment-we-meet.py
@@ -0,0 +1,34 @@
+# Setting
+A = [0]
+B = [0]
+
+n, m = map(int, input().split())
+
+# For A
+for _ in range(n):
+    dir, time = input().split()
+    time = int(time)
+    for _ in range(time):
+        position = A[-1]
+        if dir == 'R':
+            A.append(position + 1)
+        else:
+            A.append(position - 1)
+
+# For B
+for _ in range(m):
+    dir, time = input().split()
+    time = int(time)
+    for _ in range(time):
+        position = B[-1]
+        if dir == 'R':
+            B.append(position + 1)
+        else:
+            B.append(position - 1)
+
+length = len(A) if len(A) >= len(B) else len(B)
+
+for i in range(1, length):
+    if A[i] == B[i]:
+        print(i)
+        break
