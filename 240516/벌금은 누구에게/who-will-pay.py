36600789b060e298e8c3eabcf8006fc82d4259d3
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-05-16 12:58:43 +0900

    Title: 벌금은 누구에게, Time: 141ms, Memory: 25MB, Status: Passed - CodeTree

--- 240516/벌금은 누구에게/who-will-pay.py
+++ 240516/벌금은 누구에게/who-will-pay.py
@@ -1,14 +1,12 @@
 n, m, k = map(int, input().split())
 student = [0 for _ in range(n+1)]
 
-for _ in range(m):
-    panalty = int(input())
-    student[panalty] += 1
-
 answer = -1
-for i in range(1, len(student)):
-    if student[i] >= k:
-        answer = i
+for _ in range(m):
+    penalty = int(input())
+    student[penalty] += 1
+    if student[penalty] == k:
+        answer = penalty
         break
 
 print(answer)
