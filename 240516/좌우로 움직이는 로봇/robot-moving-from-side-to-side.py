d27af82f6f47d21b667d516954a6193b561959cb
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-05-16 18:01:53 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240516/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,41 @@
+A = [0]
+B = [0]
+
+a_pos = 0
+b_pos = 0
+
+n, m = map(int, input().split())
+
+for _ in range(n):
+    time, dir = input().split()
+    time = int(time)
+    for _ in range(time):
+        if dir == 'R':
+            a_pos += 1
+            A.append(a_pos)
+        else:
+            a_pos -= 1
+            A.append(a_pos)
+            
+for _ in range(m):
+    time, dir = input().split()
+    time = int(time)
+    for _ in range(time):
+        if dir == 'R':
+            b_pos += 1
+            B.append(b_pos)
+        else:
+            b_pos -= 1
+            B.append(b_pos)
+
+answer = 0
+length = len(A) if len(A) <= len(B) else len(B)
+dum = len(B) - length if len(A) <= len(B) else len(A) - length
+for i in range(1, length):
+    if A[i-1] != B[i-1] and A[i] == B[i]:
+        answer += 1
+for j in range(length, length + dum):
+    if A[length-1] == B[j]:
+        answer += 1
+
+print(answer)
