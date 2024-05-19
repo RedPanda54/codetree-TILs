0009c7799d3452a98d729494e5a79c31b0988964
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-05-19 17:26:09 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 145ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240519/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -0,0 +1,23 @@
+from collections import defaultdict
+
+N, K, P, T = map(int, input().split())
+
+people = [0] * (N + 1)
+handshake = defaultdict(int)
+people[P] = 1
+infos = [((input().split())) for _ in range(T)]
+infos.sort(key = lambda x: int(x[0]))
+
+for t, x, y in infos:
+    t, x, y = int(t), int(x), int(y)
+    if people[x] == 1 and handshake[x] < K:
+        people[y] = 1
+        handshake[x] += 1
+    elif people[y] == 1 and handshake[y] < K:
+        people[x] = 1
+        handshake[y] += 1
+    elif people[x] == 1 and people[y] == 1:
+        handshake[x] += 1
+        handshake[y] += 1
+
+print(''.join(str(num) for num in people[1:]))
