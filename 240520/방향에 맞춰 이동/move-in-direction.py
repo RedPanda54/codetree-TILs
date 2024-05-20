57715364d69ee2f1fdc8e17bf33610f08f508e9e
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-05-20 21:41:46 +0900

    Title: 방향에 맞춰 이동, Time: 222ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240520/방향에 맞춰 이동/move-in-direction.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+x, y = 0, 0
+dir = [(1, 0), (-1, 0), (0, -1), (0, 1)] # 동서남북
+
+for _ in range(n):
+    str, distance = input().split()
+    distance = int(distance)
+    
+    dx, dy = 0, 0
+    if str == 'E':
+        dx, dy = dir[0]
+    elif str == 'W':
+        dx, dy = dir[1]
+    elif str == 'S':
+        dx, dy = dir[2]
+    else: # str == 'N'
+        dx, dy = dir[3]
+    
+    dx *= distance
+    dy *= distance
+    x += dx
+    y += dy
+
+print(x, y)
