e399cbbb9aa2d7a7dec7370a1e19990978247f50
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-08-01 22:55:11 +0900

    Title: 특정 숫자 도달하기, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240801/특정 숫자 도달하기/reaching-specific-number.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] arr = new int[10];
+
+        int count = 0;
+        int sum = 0;
+        double average = 0.0;
+
+        for (int i = 0; i < 10; i++) {
+            arr[i] = input.nextInt();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            if (arr[i] >= 250) {
+                break;
+            }
+            count++;
+            sum += arr[i];
+        }
+        average = (double) sum / count;
+
+        System.out.printf("%d, %.1f", sum, average);
+    }
+}
