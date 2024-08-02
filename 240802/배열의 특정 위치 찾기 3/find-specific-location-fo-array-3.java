ba1f62be8383ad5505ad1f4107ec47f664db3451
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-08-02 21:19:18 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 124ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240802/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
+++ 240802/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.java
@@ -13,7 +13,14 @@ public class Main {
                 break;
             }
         }
-
-        System.out.print(arr[temp - 1] + arr[temp - 2] + arr[temp - 3]);
+        if (temp >= 3) {
+            System.out.print(arr[temp - 1] + arr[temp - 2] + arr[temp - 3]);   
+        } else if (temp == 2) {
+            System.out.print(arr[temp - 1] + arr[temp - 2]);
+        } else if (temp == 1) {
+            System.out.print(arr[temp - 1]);
+        } else {
+            System.out.print(0);
+        }
     }
 }
