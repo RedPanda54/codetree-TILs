8030d5f0c4d662b1710fab9e43fb590d53328cf7
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-08-02 21:53:26 +0900

    Title: 특정 위치의 문자 2, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

--- 240802/특정 위치의 문자 2/char-in-specific-location-2.java
+++ 240802/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[] arr = new int[10];
+        char[] arr = new char[10];
         for (int i = 0; i < 10; i++) {
-            arr[i] = input.nextInt();
+            arr[i] = input.next().charAt(0);
         }
-        System.out.print(arr[1] + arr[4] + arr[7]);
+        System.out.printf("%c %c %c",arr[1], arr[4], arr[7]);
     }
 }
