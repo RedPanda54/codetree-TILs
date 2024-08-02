0085c1a6b9dbc60b1d6f32f9c182b1f975cd141c
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-08-02 21:52:05 +0900

    Title: 특정 위치의 문자 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240802/특정 위치의 문자 2/char-in-specific-location-2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[] arr = new int[10];
+        for (int i = 0; i < 10; i++) {
+            arr[i] = input.nextInt();
+        }
+        System.out.print(arr[1] + arr[4] + arr[7]);
+    }
+}
