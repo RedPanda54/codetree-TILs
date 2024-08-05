b9d971175f4d435aa27f9f2c40742e6a45063c0a
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-08-05 15:28:03 +0900

    Title: 배열 놀이, Time: 151ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240805/배열 놀이/play-with-array.java
+++ 240805/배열 놀이/play-with-array.java
@@ -5,35 +5,35 @@ public class Main {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         int q = input.nextInt();
-
         int[] nums = new int[n];
+        
         for (int i = 0; i < n; i++) {
             nums[i] = input.nextInt();
         }
 
-        int q1, q2, q3;
+        int qType, a, b;
         for (int i = 0; i < q; i++) {
             // 질의 번호를 입력 받음
-            q1 = input.nextInt();
+            qType = input.nextInt();
 
             // 질의 번호에 따라 다른 명령문을 실행
-            if (q1 == 1) {
-                q2 = input.nextInt();
-                System.out.println(nums[q2 - 1]);
-            } else if (q1 == 2) {
-                int tmp = 0;
-                q2 = input.nextInt();
+            if (qType == 1) {
+                a = input.nextInt();
+                System.out.println(nums[a - 1]);
+            } else if (qType == 2) {
+                int index = -1;
+                a = input.nextInt();
                 for (int j = 0; j < n; j++) {
-                    if (nums[j] == q2) {
-                        tmp = j + 1;
+                    if (nums[j] == a) {
+                        index = j;
                         break;
                     }
                 }
-                System.out.println(tmp);
-            } else {    // q1 == 3
-                q2 = input.nextInt();
-                q3 = input.nextInt();
-                for (int j = q2 - 1; j <= q3 - 1; j++) {
+                System.out.println(index);
+            } else {    // qType == 3
+                a = input.nextInt();
+                b = input.nextInt();
+                for (int j = a - 1; j <= b - 1; j++) {
                     System.out.print(nums[j] + " ");
                 }
                 System.out.println();
