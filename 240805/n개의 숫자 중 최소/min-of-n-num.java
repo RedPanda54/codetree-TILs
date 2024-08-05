95aa1abbe2a0e661319726ba9936b28bdd813513
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-08-05 16:34:43 +0900

    Title: n개의 숫자 중 최소, Time: 173ms, Memory: 10MB, Status: Passed - CodeTree

--- 240805/n개의 숫자 중 최소/min-of-n-num.java
+++ 240805/n개의 숫자 중 최소/min-of-n-num.java
@@ -6,7 +6,7 @@ public class Main {
         int n = input.nextInt();
         int[] arr = new int[n];
         
-        int minValue = 429496729;
+        int minValue = 2147483647;
         for (int i=0; i<n; i++) {
             arr[i] = input.nextInt();
             if (arr[i] < minValue) {
