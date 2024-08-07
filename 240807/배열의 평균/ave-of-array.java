3b7e0e2391c8c8b5151244bb5a02d58e2db8b28b
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-08-07 22:17:53 +0900

    Title: 배열의 평균, Time: 146ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240807/배열의 평균/ave-of-array.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int[][] arr = new int[2][4];
+
+        // 2차원 배열의 원소 입력받기
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 4; j++)
+                arr[i][j] = input.nextInt();
+
+        double widAverage = 0.0;
+        double heiAverage = 0.0;
+        double average = 0.0;
+
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+                widAverage += arr[i][j];
+            }
+            System.out.print(widAverage / 4 + " ");
+            widAverage = 0.0;
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 2; j++) {
+                heiAverage += arr[j][i];
+            }
+            System.out.print(heiAverage / 2 + " ");
+            heiAverage = 0.0;
+        }
+        
+        System.out.println();
+
+        for (int i = 0; i < 2; i++)
+            for (int j = 0; j < 4; j++)
+                average += arr[i][j];
+        
+        System.out.print(average / 8);
+    }
+}
