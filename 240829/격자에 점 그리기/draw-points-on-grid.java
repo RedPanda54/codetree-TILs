ee777179a8cc32ce5dda70d68bd4754761f2fd97
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-08-29 21:32:32 +0900

    Title: 격자에 점 그리기, Time: 171ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240829/격자에 점 그리기/draw-points-on-grid.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int m = input.nextInt();
+
+        int[][] arr = new int[n][n];
+
+        for (int i = 1; i <= m; i++) {
+            int r = input.nextInt();
+            int c = input.nextInt();
+            arr[r - 1][c - 1] = i;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        input.close();
+    }
+}
