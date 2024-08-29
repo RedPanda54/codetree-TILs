353327ba9e159f5e1c5e61ef04b4cf771bfb51e9
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-08-29 21:35:08 +0900

    Title: 문자열 두번 출력하기, Time: 117ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240829/문자열 두번 출력하기/print-string-twice.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str = input.next();
+        
+        System.out.println(str);
+        System.out.println(str);
+        
+        input.close();
+    }
+}
