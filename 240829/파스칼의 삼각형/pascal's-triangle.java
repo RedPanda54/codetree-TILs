9e61d697efeb65475f75710d21b004ff66eb6636
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-08-29 21:20:36 +0900

    Title: 파스칼의 삼각형, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240829/파스칼의 삼각형/pascal's-triangle.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[][] array = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            array[i][0] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < n; j++) {
+                array[i][j] = array[i - 1][j] + array[i - 1][j - 1];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (array[i][j] != 0) {
+                    System.out.print(array[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+
+        input.close();
+    }
+}
