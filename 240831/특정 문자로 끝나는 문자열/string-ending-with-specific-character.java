817a93ddf3caa6f2ce3e575e0c922ad9904133d3
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-08-31 20:25:02 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240831/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
+++ 240831/특정 문자로 끝나는 문자열/string-ending-with-specific-character.java
@@ -4,18 +4,22 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String[] arr = new String[10];
-
+        
         for (int i = 0; i < 10; i++)
             arr[i] = input.next();
 
         char alphbat = input.next().charAt(0);
 
+        boolean check = true;
         for (int i = 0; i < 10; i++) {
-            if (arr[i].charAt(arr[i].length() - 1) == alphbat)
+            if (arr[i].charAt(arr[i].length() - 1) == alphbat) {
                 System.out.println(arr[i]);
-            else 
-                System.out.println("None");
+                check = false;
+            }
         }
+        
+        if (check)
+            System.out.println("None");
 
         input.close();
     }
