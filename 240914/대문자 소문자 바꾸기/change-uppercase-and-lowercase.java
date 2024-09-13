28717b8ccd31314b61ea4cd4161eb97a05432186
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-09-14 08:58:00 +0900

    Title: 대문자 소문자 바꾸기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240914/대문자 소문자 바꾸기/change-uppercase-and-lowercase.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static boolean isUpper(char x) {
+        if ('A' <= x && x <= 'Z')
+            return true;
+        return false;
+    }
+
+    public static boolean isLower(char x) {
+        if ('a' <= x && x <= 'z')
+            return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str = input.next();
+        int len = str.length();
+
+        String result = "";
+        for (int i = 0; i < len; i++) {
+            char temp = str.charAt(i);
+            if ('A' <= temp && temp <= 'z') {
+                if (isUpper(temp))
+                    result += (char) (temp - 'A' + 'a');
+                else if (isLower(temp))
+                    result += (char) (temp - 'a' + 'A');
+            }
+        }
+
+        System.out.print(result);
+
+        input.close();
+    }
+}
