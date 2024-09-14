5fab1c65faf5ef7e074839cd9ba6b4557f5d4645
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-09-14 09:37:08 +0900

    Title: 정수만 추출하기, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240914/정수만 추출하기/extract-only-integers.java
+++ 240914/정수만 추출하기/extract-only-integers.java
@@ -1,37 +1,35 @@
 import java.util.*;
 
 public class Main {
-    public static boolean isInteger(char a) {
-        if ('0' <= a && a <= '9') {
-            return true;
+    // 문자열의 앞부분에서 정수 부분을 추출하는 함수
+    public static int extractInteger(String str) {
+        StringBuilder number = new StringBuilder();
+        
+        // 문자열의 앞부분에서 숫자만 추출
+        for (int i = 0; i < str.length(); i++) {
+            if (Character.isDigit(str.charAt(i))) {
+                number.append(str.charAt(i));
+            } else {
+                break; // 숫자가 아닌 문자를 만나면 중단
+            }
         }
-        return false;
+        // 추출한 문자열을 정수로 변환
+        return Integer.parseInt(number.toString());
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
+        // 두 개의 문자열 입력
         String str1 = input.next();
         String str2 = input.next();
-        String int1 = "";
-        String int2 = "";
-
-        int i = 0;
-        char temp1 = str1.charAt(i);
-        while (isInteger(temp1)) {
-            int1 += temp1;
-            temp1 = str1.charAt(++i);
-        }
-        int result1 = Integer.parseInt(int1);
-
-        i = 0;
-        char temp2 = str2.charAt(i);
-        while (isInteger(temp2)) {
-            int2 += temp2;
-            temp2 = str2.charAt(++i);
-        }
-        int result2 = Integer.parseInt(int2);
-
-        System.out.println(result1 + result2);
+        
+        // 각 문자열에서 정수 부분 추출
+        int num1 = extractInteger(str1);
+        int num2 = extractInteger(str2);
+        
+        // 두 정수의 합 출력
+        System.out.println(num1 + num2);
         
         input.close();
     }
