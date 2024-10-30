db5329019f43b1c0255a62d6f391287ca11442ce
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-10-30 16:25:40 +0900

    Title: 규칙을 찾아 출력하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 241030/규칙을 찾아 출력하기/find-a-rule-and-print.java
+++ 241030/규칙을 찾아 출력하기/find-a-rule-and-print.java
@@ -7,9 +7,13 @@ public class Main {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
+                // n칸의 정사각형에서 가장 윗줄과 오른쪽 줄.
                 if (i == 0 || j == n - 1) {
                     System.out.print("* ");
-                } else {
+                } 
+                
+                // 나머지 칸의 규칙. 
+                else {  
                     if (i > j) {
                         System.out.print("* ");
                     } else {
