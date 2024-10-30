e682aaa08d48ea4bbe841e817270e700c252b85b
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-10-30 15:50:01 +0900

    Title: 별표 출력하기 13, Time: 156ms, Memory: 10MB, Status: Passed - CodeTree

--- 241030/별표 출력하기 13/print-start-13.java
+++ 241030/별표 출력하기 13/print-start-13.java
@@ -4,43 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        int a = n;
-        int b = 1;
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                for (int j = 0; j < a; j++) {
+        for (int i = 0; i < 2 * n; i++) {
+            if (i % 2 == 1) {
+                for (int k = 0; k < 1 + i / 2; k++)
                     System.out.print("* ");
-                }
-                a -= 1;
-                System.out.println();
-            }
-            else {
-                for (int j = 0; j < b; j++) {
+            } else {
+                for (int k = 0; k < n - i / 2; k++)
                     System.out.print("* ");
-                }
-                b += 1;
-                System.out.println();
             }
-        }
-
-        a += 1;
-        b -= 1;
-
-        for (int i = n - 1; i >= 0; i--) {
-            if (i % 2 == 0) {
-                for (int j = 0; j < a; j++) {
-                    System.out.print("* ");
-                }
-                a += 1;
-                System.out.println();
-            }
-            else {
-                for (int j = 0; j < b; j++) {
-                    System.out.print("* ");
-                }
-                b -= 1;
-                System.out.println();
-            }
+            System.out.println();
         }
         input.close();
     }
