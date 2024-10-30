d904e2a22b0c381e87363152f635edad7272ea75
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-10-30 16:30:12 +0900

    Title: 열 단위로 반복하여 출력, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

--- 241030/열 단위로 반복하여 출력/print-in-column.java
+++ 241030/열 단위로 반복하여 출력/print-in-column.java
@@ -4,13 +4,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        int a = 1;
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print(a);
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print(i + 1);
             }
-            a++;
             System.out.println();
         }
 
