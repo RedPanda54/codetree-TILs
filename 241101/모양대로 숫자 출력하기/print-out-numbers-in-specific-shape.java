1312938cfe136b058472ac1e640e263e7bb9c4d4
Kim myeong_hwi <103114227+RedPanda54@users.noreply.github.com>
2024-11-01 18:17:20 +0900

    Title: 모양대로 숫자 출력하기, Time: 227ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241101/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j < n; j++) {
+                System.out.print(n - j + " ");
+            }
+            System.out.println();
+        }
+
+        input.close();
+    }
+}
